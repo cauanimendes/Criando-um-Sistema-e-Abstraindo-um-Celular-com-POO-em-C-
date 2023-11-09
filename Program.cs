6fa24a0132cd8aef0292cd3ab4e111b4684d4f7f
Cauãni Mendes <119821164+cauanimendes@users.noreply.github.com>
2023-11-09 17:20:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Nokia:");
+Nokia nokia = new Nokia ("(11) 9876-6546", "C21 Plus", "12356709", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("YouTube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone:");
+Iphone iphone = new Iphone ("11 9768-0921", "13 Pro", "67895408", 256);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Netflix");
